113938fe0f6543fbc9ef9e0749eadedc272e7303
Taylor Mohney <taylormohneytech@gmail.com>
2024-09-24 20:10:43 -0700

    no api prefix

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -59,7 +59,7 @@ function App() {
   };
 
   const updateUserMetrics = (isCorrect) => {
-    fetch(`/api/metrics?userId=${userId}`)
+    fetch(`/metrics?userId=${userId}`)
         .then(response => {
             if (!response.ok) {
                 throw new Error(`HTTP error! status: ${response.status}`);
@@ -79,7 +79,7 @@ function App() {
                 incorrectAnswers: newIncorrectAnswers,
             };
 
-            return fetch(`${process.env.REACT_APP_API_URL}/api/metrics`, {
+            return fetch(`${process.env.REACT_APP_API_URL}/metrics`, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -122,7 +122,7 @@ function App() {
   
     console.log('Sending performance data:', performanceData);
 
-    fetch(`${process.env.REACT_APP_API_URL}/api/performance`, {
+    fetch(`${process.env.REACT_APP_API_URL}/performance`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -177,7 +177,7 @@ function App() {
     const decodedToken = jwtDecode(idToken); 
     console.log('Decoded Token:', decodedToken);
 
-    fetch(`${process.env.REACT_APP_API_URL}/api/login`, {
+    fetch(`${process.env.REACT_APP_API_URL}/login`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
